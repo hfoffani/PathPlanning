35efadbad0189e997a1f14b3d173964969acfb09
hfoffani <hfoffani@gmail.com>
2017-09-28 11:47:03 +0200

    Adjust some constants
    Increase rear safety space before changing lanes.
    Increase acceleration for better responsiveness.

--- src/constants.h
+++ src/constants.h
@@ -9,17 +9,17 @@
 #define BUSYAHEADMIN 0
 #define BUSYAHEADMAX 30
 
-#define BUSYCHANGEMIN -15
+#define BUSYCHANGEMIN -20
 #define BUSYCHANGEMAX 35
 
 #define PATHPOINTS 50
 
-#define POINTSPEED 0.02     // car moves to a new waypoint every 20ms
-#define NOJERKACC 0.224     // 5m/s per ??
-#define SAFETYMARGIN .9     // car ahead speed limit befere braking
+#define POINTSPEED 0.02             // car moves to a new waypoint every 20ms
+#define NOJERKACC (0.224*1.5)       // 7.5m/s
+#define SAFETYMARGIN .9             // car ahead speed limit befere braking
 
-#define MPH2MS(x) (x/2.24)  // mph to m/s
-#define MS2MPH(x) (x*2.24)  // m/s to mph
+#define MPH2MS(x) (x/2.24)          // mph to m/s
+#define MS2MPH(x) (x*2.24)          // m/s to mph
 
 #define MAXVELOCITY 49.70
 
